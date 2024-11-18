4a61357ac108a2717a6889291d9c20c967d43431
Marco Frescas <91026875+mfrescas@users.noreply.github.com>
2024-11-17 18:47:46 -0600

    added route to team + other things

--- server/routes/Team.js
+++ server/routes/Team.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { Team } = require("../models");
-const { all } = require("./test");
 
 router.get("/", async (req, res) => {
     const allTeams = await Team.findAll();
