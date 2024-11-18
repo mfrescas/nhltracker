2c818f41d32bd15c74e739d576791b001c3f32ed
Marco Frescas <91026875+mfrescas@users.noreply.github.com>
2024-11-17 22:31:29 -0600

    Enabled cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
 const db = require('./models');
 
+app.use(express.json());
+app.use(cors());
 
 // Routers 
 const teamsRouter = require ('./routes/teams');
